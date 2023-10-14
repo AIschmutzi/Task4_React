9e959bf15f6be8025f468223204bb375a3741a11
itsaradh <145046994+itsaradh@users.noreply.github.com>
2023-10-14 20:47:15 +0530

    fourth commmit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import UserTable from './Components/UserTable';
+
+
+function App() {
+  return (
+   <UserTable/>
+  );
+}
+
+export default App;
